0c9f8f61d05701ea6902fde947a4d436ef469928
Stephen Triphahn <stephentriphahn@gmail.com>
2019-03-25 22:42:31 -0400

    create basic npm module

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-const fs = require('fs');
 const through = require('through2');
 const split = require('split2');
-const pump = require('pump');
+const pumpify = require('pumpify')
 
-const fileStream = fs.createReadStream(process.argv[2]);
 
-function parseWith(delimiter) {
-    var fields = null;
+function parseLinesWith(delimiter) {
+    var fields = null; // init as null, until first line of file containig fields is parsed. 
     delimiter = delimiter || ","
     return through.obj((data, enc, cb) => {
         const line = data.toString().split(delimiter).map(d => d.trim());
         if (!fields) {
-            fields = line;
-            return cb(null, null);
+            fields = line; //
+            return cb(null, null); // essentially remove fields from stream
         }
         var obj = {};
 
@@ -24,17 +22,15 @@ function parseWith(delimiter) {
     });
 }
 
-function logObject() {
+function objectToJSONStream() {
     return through.obj((data, enc, cb) => {
-        return cb(null, JSON.stringify(data))
-    })
+        cb(null, JSON.stringify(data))
+    });
 }
 
-pump(fileStream, split(), parseWith(), logObject(), process.stdout, (err) => {
-    console.error("An error occured!");
-    console.error(err.message);
-});
-// fileStream
-//     .pipe(split())
-//     .pipe(parseCSV())
-//     .pipe(process.stdout);
+//Creates a pumpified stream, parses lines with delimiter and writes to object stream
+function createDelimitedPipeline(delimiter) {
+    return pumpify(split(), parseLinesWith(delimiter), objectToJSONStream());
+}
+
+module.exports = createDelimitedPipeline;
